5ec32ea24874a3605589368f62b775cd5ab1b147
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-18 15:22:14 +0100

    removed simple counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,11 @@ import { useState, useEffect } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
-  useEffect(()=>{
-    const intv = setInterval(()=>{
-      setCount(count + 1)
-    }, 1000)
 
-    // clearInterval
-    return () => clearInterval(intv)
-  }, [count]);
 
   return (
     <>
-      <h1 className='flex justify-center items-center mx-auto'>{count}</h1>
+      
     </>
   )
 }
