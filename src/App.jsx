2f7525a26edc8edd76efd1dfbf1c8d0323135869
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-19 09:12:53 +0100

    re-arraged synthax

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,54 @@ import { useState, useEffect } from 'react'
 import Countdown from './Components/CountdownTimer/Countdown'
 
 function App() {
+// Create state for components of timer
+const [timerDays, setTimerDays] = useState();
+const [timerHours, setTimerHours] = useState();
+const [timerMinutes, setTimerMinutes] = useState();
+const [timerSeconds, setTimerSeconds] = useState();
 
+let interval;
+
+// Get Time!
+const startTime = ()=>{
+    const countDownDate = new Date("").getTime();
+
+    interval = setInterval(()=>{
+        // Current Time
+        const now = new Date().getTime();
+        
+        // Difference from countdown date to Now
+        const difference = countDownDate - now;
+        
+        // Remaining Days
+        const days = Math.floor(difference / (24 * 60 * 60 * 1000));
+        // Remaining Hours
+        const hours = Math.floor(difference % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
+        // Remaining Minutes
+        const minutes = Math.floor(difference % (24 * 60 * 60 * 1000) / (60 * 1000));
+        // Remaining Seconds
+        const seconds = Math.floor(difference % (24 * 60 * 60 * 1000) / 1000); 
+
+        // Stop Timer using clearInterval
+        if (difference < 0){
+            clearInterval(interval.current);
+        }else{
+            setTimerDays(days);
+            setTimerHours(hours);
+            setTimerMinutes(minutes);
+            setTimerSeconds(seconds);
+        }
+
+    });
+};
 
 
   return (
     <>
-      <Countdown />
+      <Countdown timerDays={timerDays} 
+      timerHours={timerHours} 
+      timerMinutes={timerMinutes} 
+      timerSeconds={timerSeconds} />
     </>
   )
 }
--- src/Components/CountdownTimer/Countdown.jsx
+++ src/Components/CountdownTimer/Countdown.jsx
@@ -1,47 +1,7 @@
 import {useState} from 'react'
 
-const Countdown = () => {
-    // Create state for components of timer
-    const [timerDays, setTimerDays] = useState();
-    const [timerHours, setTimerHours] = useState();
-    const [timerMinutes, setTimerMinutes] = useState();
-    const [timerSeconds, setTimerSeconds] = useState();
-
-    let interval;
-
-    // Get Time!
-    const startTime = ()=>{
-        const countDownDate = new Date("").getTime();
-
-        interval = setInterval(()=>{
-            // Current Time
-            const now = new Date().getTime();
-            
-            // Difference from countdown date to Now
-            const difference = countDownDate - now;
-            
-            // Remaining Days
-            const days = Math.floor(difference / (24 * 60 * 60 * 1000));
-            // Remaining Hours
-            const hours = Math.floor(difference % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
-            // Remaining Minutes
-            const minutes = Math.floor(difference % (24 * 60 * 60 * 1000) / (60 * 1000));
-            // Remaining Seconds
-            const seconds = Math.floor(difference % (24 * 60 * 60 * 1000) / 1000); 
-
-            // Stop Timer using clearInterval
-            if (difference < 0){
-                clearInterval(interval.current);
-            }else{
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-                setTimerSeconds(seconds);
-            }
-
-        });
-    };
-
+const Countdown = ({timerDays, timerHours, timerMinutes, timerSeconds}) => {
+    
   return (
     <div className='flex justify-center items-center mx-auto w-screen h-screen p-8'>
         {/* App Container */}
@@ -62,10 +22,10 @@ const Countdown = () => {
 };
 
 Countdown.defaultProps = {
-    timerDays: 10,
-    timerHours: 10,
-    timerMinutes: 10,
-    timerSeconds: 10,
+    timerDays: '--',
+    timerHours: "--",
+    timerMinutes: "--",
+    timerSeconds: "--",
 };
 
 export default Countdown
